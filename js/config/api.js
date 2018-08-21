9248c0a565de26d66010cb4c31d058d42e6a5add
Andrei Egorov <andreyegorov1992@gmail.com>
2018-08-20 23:11:57 -0700

    Add R10 app, initialize Apollo, and Style about page

---
+++ js/config/api.js
@@ -0,0 +1,16 @@
+import ApolloClient from "apollo-boost";
+
+
+import { Query } from "react-apollo";
+
+const Client = new ApolloClient({
+  uri: "https://api.graph.cool/simple/v1/cjh2hph6n6njl0108f6n59j3j"
+  
+});
+
+
+
+export default Client
+
+
+
---
+++ js/screens/About/About.js
@@ -0,0 +1,34 @@
+//rnc
+
+import React from 'react'
+import styles from './styles'
+// const rTenImage = require('./../../assets/r10_logo3x.png') 
+
+import {Text, View, ScrollView, Image} from "react-native"
+
+const About = (props) => {
+    console.log("About",props.data.allConducts)
+    return( 
+        
+        <ScrollView >
+        <View style={styles.container}>
+        <Image style={styles.image} source={require("./../../assets/r10_logox.png")}/>
+        <Text style={styles.plainText}>R10 is a conference that focuses on just about any topic related to dev.</Text>
+        <Text style={styles.headings}>Date and Venue</Text>
+        <Text style={styles.plainText}>R10 conference will take place on Tuesday June 27, 2019 in Vancouver, BC.</Text>
+        <Text style={styles.headings}>Code of Conduct</Text>
+       {props.data.allConducts.map((object,index) => { 
+            return(
+                <View key={index}>
+                <Text style={styles.sectionTitle}>{object.title}</Text>
+         <Text style={styles.plainText} >{object.description}</Text></View>)
+         }
+        
+        )}
+        </View>
+        </ScrollView>
+        
+       
+    )
+}
+export default About

6d2bb5a70ed8f4157b19f8220cb774792206bead
Andrei Egorov <andreyegorov1992@gmail.com>
2018-08-24 14:55:11 -0700

    Add info on session Screen

--- js/screens/Session/Session.js
+++ js/screens/Session/Session.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react'
-import {View, Text} from 'react-native'
+import {View, Text, Image} from 'react-native'
 
 
-const Session = () => {
+const Session = ({data}) => {
+  console.log("SESSION", data )
+  console.log(data.Session.id)
+
   return(
     <View>
-        <Text> I am A Session component</Text>
+        <Text>{data.Session.location}</Text>
+        <Text>{data.Session.title}</Text>
+        <Text>{data.Session.startTime}</Text>
+        <Text>{data.Session.description}</Text>
+        <Text>Presented by:</Text>
+        
+        <Image
+        style={{width: 50, height: 50}}
+        source={{uri:data.Session.speaker.image}}></Image>
+        <Text>{data.Session.speaker.name}</Text>
     </View>
   )
   
